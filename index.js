cc4ad49eafac546b9e476900a0b20d1727e28db9
zolomohan <mohanzolomon@gmail.com>
2020-12-31 20:44:57 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+let speech = new SpeechSynthesisUtterance();
+speech.lang = "en";
+speech.rate = 1;
+speech.volume = 1;
+speech.pitch = 1;
+
+let voices = [];
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+  let voiceSelect = document.querySelector("#voices");
+  voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+};
+
+function updateVoice() {
+  speech.voice = voices[document.querySelector("#voices").value];
+}
+
+document.querySelector('#talk').addEventListener('click', () => {
+  speech.text = document.querySelector("textarea").value;
+  window.speechSynthesis.speak(speech);
+});
+
+document.querySelector('#pause').addEventListener('click', () => {
+  window.speechSynthesis.pause();
+});
+
+document.querySelector('#resume').addEventListener('click', () => {
+  window.speechSynthesis.resume();
+});
+
+document.querySelector('#cancel').addEventListener('click', () => {
+  window.speechSynthesis.cancel();
+});
