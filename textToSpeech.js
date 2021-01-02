989d168ea8c5442d2d531171d2bc6debf586446e
zolomohan <mohanzolomon@gmail.com>
2021-01-02 13:47:38 +0530

    Add Range Slider for Volume, Rate and Pitch

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,17 +1,31 @@
 let speech = new SpeechSynthesisUtterance();
 speech.lang = "en";
-speech.rate = 1;
-speech.volume = 1;
-speech.pitch = 1;
 
 let voices = [];
-
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
   let voiceSelect = document.querySelector("#voices");
   voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
 };
+
+document.querySelector("#rate").addEventListener("input", () => {
+  const rate = document.querySelector("#rate").value;
+  speech.rate = rate;
+  document.querySelector("#rate-label").innerHTML = rate;
+});
+
+document.querySelector("#volume").addEventListener("input", () => {
+  const volume = document.querySelector("#volume").value;
+  speech.volume = volume;
+  document.querySelector("#volume-label").innerHTML = volume;
+});
+
+document.querySelector("#pitch").addEventListener("input", () => {
+  const pitch = document.querySelector("#pitch").value;
+  speech.pitch = pitch;
+  document.querySelector("#pitch-label").innerHTML = pitch;
+});
 
 document.querySelector("#voices").addEventListener("change", () => {
   speech.voice = voices[document.querySelector("#voices").value];
