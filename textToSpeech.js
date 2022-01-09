fde8343559091926f3b15e1373654fe331329e9e
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-01-09 16:32:56 +0100

    Add comments

--- textToSpeech.js
+++ textToSpeech.js
@@ -1,7 +1,12 @@
+// Initialize new SpeechSynthesisUtterance object
 let speech = new SpeechSynthesisUtterance();
+
+// Set speech Language
 speech.lang = "en";
 
+ // global array of available voices
 let voices = [];
+
 window.speechSynthesis.onvoiceschanged = () => {
   voices = window.speechSynthesis.getVoices();
   speech.voice = voices[0];
@@ -10,40 +15,62 @@ window.speechSynthesis.onvoiceschanged = () => {
 };
 
 document.querySelector("#rate").addEventListener("input", () => {
+  // Get rate Value from the input
   const rate = document.querySelector("#rate").value;
+
+  // Set rate property of the SpeechSynthesisUtterance instance
   speech.rate = rate;
+
+  // Update the rate label
   document.querySelector("#rate-label").innerHTML = rate;
 });
 
 document.querySelector("#volume").addEventListener("input", () => {
+  // Get volume value from the input
   const volume = document.querySelector("#volume").value;
+
+  // Set volume property of the SpeechSynthesisUtterance instance
   speech.volume = volume;
+
+  // Update the volume label
   document.querySelector("#volume-label").innerHTML = volume;
 });
 
 document.querySelector("#pitch").addEventListener("input", () => {
+  // Get pitch value from the input
   const pitch = document.querySelector("#pitch").value;
+
+  // Set pitch property of the SpeechSynthesisUtterance instance
   speech.pitch = pitch;
+
+  // Update the pitch label
   document.querySelector("#pitch-label").innerHTML = pitch;
 });
 
 document.querySelector("#voices").addEventListener("change", () => {
+  // On Voice change, use the value of the select menu (which is the index of the voice in the global voice array)
   speech.voice = voices[document.querySelector("#voices").value];
 });
 
 document.querySelector("#start").addEventListener("click", () => {
+  // Set the text property with the value of the textarea
   speech.text = document.querySelector("textarea").value;
+
+  // Start Speaking
   window.speechSynthesis.speak(speech);
 });
 
 document.querySelector("#pause").addEventListener("click", () => {
+  // Pause the speechSynthesis instance
   window.speechSynthesis.pause();
 });
 
 document.querySelector("#resume").addEventListener("click", () => {
+  // Resume the paused speechSynthesis instance
   window.speechSynthesis.resume();
 });
 
 document.querySelector("#cancel").addEventListener("click", () => {
+  // Cancel the speechSynthesis instance
   window.speechSynthesis.cancel();
 });
